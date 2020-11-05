f9d22d289a62674f00ccbfc7b6ccc739bd5d5c25
Kun Ho Lim <klim8940@uni.sydney.edu.au>
2020-11-05 23:17:35 +1100

    supporting for the gui in dev rn

--- converter.py
+++ converter.py
@@ -1,31 +1,33 @@
-i = 0
-with open("in3.txt", "r", encoding="utf-8") as f, open("out3.txt", "w", encoding="utf-8") as export:
-    while True:
-        line = f.readline()
-        line = line.rstrip()
-        # if line == '':
-        #     i += 1
-        #     continue
-        #
-        # if line == '#':
-        #     i += 1
-        #     continue
-        #
-        # if line == "s":
-        #     i += 1
-        #     continue
-        #
-        # if line == "j":
-        #     i += 1
-        #     continue
-        #
-        # if line == "e":
-        #     i += 1
-        #     continue
-
-        export.write('" {} "\n'.format(line))
-        # else:
-        #
+from typing import TextIO
+from appJar import gui
 
 
+def script_converter():
+    i = 0
+    file: TextIO
+    with open("in.txt", "r", encoding="utf-8") as openfileobject, open("out.txt", "w", encoding="utf-8") as export:
+        for stuff in openfileobject:
+            line = stuff.rstrip()
+            export.write('" {} "\n'.format(stuff))
+            i += 1
 
+def script_converter_file(file):
+    i = 0
+    file: TextIO
+    with open(file, "r", encoding="utf-8") as openfileobject, open("out.txt", "w", encoding="utf-8") as export:
+        for stuff in openfileobject:
+            line = stuff.rstrip()
+            export.write('" {} "\n'.format(stuff))
+            i += 1
+
+    print("process done total changed script number: " + str(i))
+
+
+if __name__ == '__main__':
+    app = gui()
+
+    app.addLabel("title", "렌파이 스크립트 변환기 0.01 by 리미티드 팩토리")
+    app.addTextArea("t1", script_converter())
+    app.addFileEntry("f1")
+
+    app.go()
