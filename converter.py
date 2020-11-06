be5e1bb50eb43e79924a0c1420b2269e710f5f2a
Kun Ho Lim <klim8940@uni.sydney.edu.au>
2020-11-06 17:10:53 +1100

    working gui
    using appjar

--- converter.py
+++ converter.py
@@ -1,33 +1,63 @@
 from typing import TextIO
 from appJar import gui
+import sys
+
+
+def script_loader():
+    file: TextIO
+    initialize: TextIO
+    with open("in.txt", "w", encoding="utf-8") as initialize:
+        # converting textbox into txt file
+        texts = app.getTextArea("t1")
+        initialize.write(texts)
+
+        app.infoBox("info1", "Script Loaded", parent=None)
 
 
 def script_converter():
     i = 0
-    file: TextIO
     with open("in.txt", "r", encoding="utf-8") as openfileobject, open("out.txt", "w", encoding="utf-8") as export:
         for stuff in openfileobject:
             line = stuff.rstrip()
             export.write('" {} "\n'.format(stuff))
             i += 1
 
-def script_converter_file(file):
-    i = 0
-    file: TextIO
-    with open(file, "r", encoding="utf-8") as openfileobject, open("out.txt", "w", encoding="utf-8") as export:
-        for stuff in openfileobject:
-            line = stuff.rstrip()
-            export.write('" {} "\n'.format(stuff))
-            i += 1
+    #for the output in the t2 box
 
-    print("process done total changed script number: " + str(i))
+    with open("out.txt", "r", encoding="utf-8") as text_output:
+        for stuff in text_output:
+            app.setTextArea("t2", stuff, end=True, callFunction=True)
+
+    info2idea = "total converted lines: " + str(i)
+    app.infoBox("info2", info2idea , parent=None)
+
+
+# def script_converter_file(file=""):
+#     i = 0
+#     file: TextIO
+#     with open(file, "r", encoding="utf-8") as openfileobject, open("out.txt", "w", encoding="utf-8") as export:
+#         for stuff in openfileobject:
+#             line = stuff.rstrip()
+#             export.write('" {} "\n'.format(stuff))
+#             i += 1
+#
+#     print("process done total changed script number: " + str(i))
+
+
+def get_entry():
+    stuff = app.getTextArea("t1")
+    print(stuff)
 
 
 if __name__ == '__main__':
     app = gui()
+    app.addLabel("title", "Renpy Script Converter by Limited Factory V0.01")
+    app.addScrolledTextArea("t1")
+    app.addScrolledTextArea("t2")
+    # app.addFileEntry("f1")
+    app.addButton('load', script_loader)
+    app.addButton('convert', script_converter)
+    app.setFont(size=16, family="Arial", underline=False)
 
-    app.addLabel("title", "렌파이 스크립트 변환기 0.01 by 리미티드 팩토리")
-    app.addTextArea("t1", script_converter())
-    app.addFileEntry("f1")
 
     app.go()
